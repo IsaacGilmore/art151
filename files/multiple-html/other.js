2cb2c423212fbb8bd35ab17b2703fbcd0e1ac413
Isaac Gilmore <isaac.l.gilmore@gmail.com>
2020-03-08 14:42:27 -0500

    p5 multiple html docs

---
+++ files/multiple-html/other.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 600);
+  background(0);
+  fill(255);
+  textSize(40);
+  textAlign(CENTER);
+}
+
+function draw() {
+  text('Third page', width/2, height/2);
+}
